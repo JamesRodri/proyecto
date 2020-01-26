7ed565173aadb579828b8799970d108f4f784548
JamesRodri <dr2259961@gmail.com>
2020-01-25 20:56:35 -0400

    aplicacionSQlite

---
+++ CrudSqlite-masterFinish/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'autores', pathMatch: 'full' },
+  {
+    path: 'autores',
+    loadChildren: () => import('./pages/autores/autores.module').then( m => m.AutoresPageModule)
+  },
+  {
+    path: 'autores/:id',
+    loadChildren: () => import('./pages/autores/autores.module').then( m => m.AutoresPageModule)
+  },
+  {
+    path: 'libros',
+    loadChildren: () => import('./pages/libros/libros.module').then( m => m.LibrosPageModule)
+  },
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
